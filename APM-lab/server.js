e7fa58746cf76c4f5098bd835dedf8d9696f3ab1
Porter Morrison <port.morrison@gmail.com>
2021-10-12 16:08:00 -0600

    critical warning

--- APM-lab/server.js
+++ APM-lab/server.js
@@ -15,6 +15,8 @@ const app = express()
 
 const path = require('path')
 
+// app.use('/style', express.static('./public/styles.css'))
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'))
   rollbar.info('html file served successfully')
@@ -38,6 +40,29 @@ app.get('/errorTest', (req, res) => {
   }
   res.status(500).send({ message: 'succesfully failed' })
 });
+
+let students = []
+
+app.post('/api/student', (req,res) => {
+    let { name } = req.body
+    name = name.trim()
+
+    const index = students.findIndex(studentName => studentName = name)
+
+    if(index === -1 && name != ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author:'Dj',type:'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.critical('No name given')
+        res.status(400).send('please provide a name.')
+    } else {
+        rollbar.warning('student already exists')
+        res.status(400).send('this student already exists')
+    }
+})
+
+app.use(rollbar.errorHandler())
 
 
 
